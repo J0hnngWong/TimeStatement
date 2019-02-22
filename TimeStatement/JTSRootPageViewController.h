3460c147635cf274fa9f0d31a6d305baf71fb2a9
王嘉宁 <wangjianing@imdada.cn>
2019-02-22 11:13:46 +0800

    [ADD] 加入页视图控制器

---
+++ TimeStatement/JTSRootPageViewController.h
@@ -0,0 +1,17 @@
+//
+//  JTSRootPageViewController.h
+//  TimeStatement
+//
+//  Created by 王嘉宁 on 2019/2/22.
+//  Copyright © 2019 Johnny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JTSRootPageViewController : UIPageViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeStatement/JTSRootPageViewController.m
@@ -0,0 +1,32 @@
+//
+//  JTSRootPageViewController.m
+//  TimeStatement
+//
+//  Created by 王嘉宁 on 2019/2/22.
+//  Copyright © 2019 Johnny. All rights reserved.
+//
+
+#import "JTSRootPageViewController.h"
+
+@interface JTSRootPageViewController ()
+
+@end
+
+@implementation JTSRootPageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
